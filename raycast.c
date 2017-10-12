00ad3264e5b1efce1eaea794fcbe047f16bc581c
FrancsicoDiaz86 <fd86@nau.edu>
2017-10-11 22:52:13 -0700

    Made the plane and sphere intersection functions (Still have bugs with the sphere and plane intersection)

--- raycast.c
+++ raycast.c
@@ -12,9 +12,10 @@ typedef struct PPMImage{
   Pixel *pix_info;
 }PPM_Image;
 
-double* raycast(V3 Rd, int count, Object *array, double* background);
+double* raycast(V3 Rd, V3 Ro, int count, Object *array, double* background);
 Pixel* render(int width, int height, Object camera, Object *array, int count);
-double sphere_intersection(V3 Rd, Object obj);
+double sphere_intersection(V3 Rd, V3 Ro, Object obj);
+double plane_intersection(V3 Rd, V3 Ro, Object obj);
 
 int main (int argc, char *argv[]){
   int obj_count = 0;
@@ -44,13 +45,15 @@ int main (int argc, char *argv[]){
 
     obj_count++; 
   }
-
+  Object camera_ptr;
   for (int i = 0; i < obj_count; i++){
     if (obj_array[i].kind == 1){
-      pix_image = render(width, height, obj_array[i], obj_array, obj_count);
+      camera_ptr = obj_array[i];
       break;
     }
   }
+
+  pix_image = render(width, height, camera_ptr, obj_array, obj_count);
   //Writing P3 to file
   fprintf(output_file, "%s\n", "P3");
 
@@ -62,81 +65,87 @@ int main (int argc, char *argv[]){
 
   //Writing image data to file
   for (int i = 0; i < width * height; i++){
-    fprintf(output_file, " %d %d %d ", pix_image[i].r, pix_image[i].g, pix_image[i].b);
+    fprintf(output_file, "%d\n", pix_image[i].r);
+    fprintf(output_file, "%d\n", pix_image[i].g);
+    fprintf(output_file, "%d\n", pix_image[i].b);
   }
   fclose(output_file);
   fclose(csv_file);
   return(0);
 }
 
-double* raycast (V3 Rd, int count, Object *array, double* background){
+double* raycast (V3 Rd, V3 Ro, int count, Object *array, double* background){
   Object closest_obj;
-  double* background_color = background;
+  double* background_color = background; 
+  double* color_array;
   double closest_t = INFINITY;
   double t;
   Object obj;
   
   for (int i = 0; i < count; i++){
     obj = array[i];
-    double t;
     if (obj.kind == 2){
-      t = sphere_intersection(Rd, obj);
-      if (t == -1){
-	return (background);
-      }
-      
+      //sphere intersection
+      t = sphere_intersection(Rd, Ro, obj);
       if (t < closest_t){
-	printf("Im here\n");
 	closest_t = t;
 	closest_obj = array[i];
       }
     }
+    
     if (obj.kind == 3){
       //plane intersection
-     
+      t = plane_intersection(Rd, Ro, obj);
+      if (t < closest_t){
+	closest_t = t;
+	closest_obj = array[i];
+      }
     }
   }
-  
-  double* color_array = closest_obj.color; 
+
+  if (closest_t == INFINITY){
+    return(background);
+  }else{
+    color_array = closest_obj.color;
+  }
   return(color_array);
 }
 
 Pixel* render (int width, int height, Object camera, Object *array, int count){
   Pixel *pix_info;
   double *color;
-  double background[3] = {1, 1, 1};
+  double background[3] = {0, 1, 0};
   double dx = (camera.width/width);
-  double dy = (camera.height/height); 
-  double top_lefty = -(camera.width/dx) - dy;
-  double top_leftx;
+  double dy = (camera.height/height);
   int current_pix = 0;
   
   pix_info = (Pixel *)malloc(sizeof(Pixel) * width * height);
   double* Pij = malloc(sizeof(double) * 3);
   double* Rd = malloc(sizeof(double) * 3);
-  
+  double* Ro = malloc(sizeof(double) * 3);
+  v3_assign(Ro, 0 , 0, -1);
   for (int i = 0; i < height; i++){
-    top_lefty += dy;
-    double Py = top_lefty + (i * dy) + (.5 * dy); 
-    top_leftx = (camera.width/width);
+    double Py = 0 - (camera.height/2) + dy * (i + 0.5);
     for (int j = 0; j < width; j++){
-      double Px = top_leftx + (j * dx) + (.5 * dx);
+      double Px = 0 - (camera.width/2) + dx * (j + 0.5);
+     
       v3_assign(Pij,
 		Px,
 		Py,
-		-10);
+		1);
       
-      double mag = sqrt(pow(Pij[0], 2) + pow(Pij[1], 2) + pow(Pij[2], 2));
+      v3_sub(Rd, Pij, Ro);
+      
+      double mag = sqrt(pow(Rd[0], 2) + pow(Rd[1], 2) + pow(Rd[2], 2));
 
      
       v3_assign(Rd,
-		Pij[0]/mag,
-		Pij[1]/mag,
-		Pij[2]/mag);
+		Rd[0]/mag,
+		Rd[1]/mag,
+		Rd[2]/mag);
 
-      top_leftx -= dx;
       
-      color = raycast(Rd, count, array, background);
+      color = raycast(Rd, Ro, count, array, background);
       
       pix_info[current_pix].r = (color[0] * 255);
       pix_info[current_pix].g = (color[1] * 255);
@@ -147,12 +156,17 @@ Pixel* render (int width, int height, Object camera, Object *array, int count){
   return(pix_info);
 }
 
-double sphere_intersection (V3 Rd, Object obj){
+double sphere_intersection (V3 Rd, V3 Ro, Object obj){
   //Rd values for x y z
   double Rdx = Rd[0];
   double Rdy = Rd[1];
   double Rdz = Rd[2];
 
+  //R0 values
+  double Rox = Ro[0];
+  double Roy = Ro[1];
+  double Roz = Ro[2];
+  
   //Sphere values for x y z and r
   double Cx = obj.position[0];
   double Cy = obj.position[1];
@@ -161,32 +175,62 @@ double sphere_intersection (V3 Rd, Object obj){
 
   //Calculating A B C for quadratic equation
   double A = pow(Rdx, 2) + pow(Rdy, 2) + pow(Rdz, 2);
-  double B = 2 * ((Rdx * (0 - Cx)) + (Rdy * (0 - Cy)) + (Rdz * (0 - Cz)));
-  double C = pow(-Cx, 2) + pow(-Cy, 2) + pow(-Cz, 2) - pow(r, 2);
+  double B = 2 * (Rdx * (Rox - Cx) + Rdy * (Roy - Cy) + Rdz * (Roz - Cz));
+  double C = ((Rox - Cx) * (Rox - Cx)) + ((Roy - Cy) * (Roy - Cy)) + ((Roz - Cz) * (Roz - Cz)) - (r * r);
 
-  printf("The value of A is: %f\n", A);
-  printf("The value of B is: %f\n", B);
-  printf("The value of C is: %f\n", C);
   //Initializing t0 t1
   double t0;
   double t1;
 
-  //Calculating (B^2-4*C)
-  double dis_part;
-  dis_part = (pow(B, 2) - 4 * C);
-
-  //Calculating discriminat for t0
-  t0 = (-B - pow(dis_part, .5))/2;
-  if (t0 < 0 ){
+  //Calculating discriminant
+  double dis;
+  dis = (B * B) - (4 * C); 
+  if (dis < 0){
     return(INFINITY);
   }
-
-  if (t0 >= 0){
+  
+  //Calculating for t0 and t1
+  t0 = (-B - sqrt(dis))/2;
+  if (t0 > 0){
     return(t0);
-  }
-  else{
-    t1 = (-B - pow(dis_part, .5))/2;
+  }else{
+    t1 = (-B + sqrt(dis))/2;
     return(t1);
   }
 }
 
+double plane_intersection (V3 Rd, V3 Ro, Object obj){
+  //value for dot product result
+  double Vd;
+  
+  //Normal values
+  double A = obj.normal[0];
+  double B = obj.normal[1];
+  double C = obj.normal[2];
+
+  //Allocating space for vector
+  double* Pn = malloc(sizeof(double) * 3);
+  
+  
+  //Making normal a unit normal
+  double mag = sqrt(pow(A, 2) + pow(B, 2) + pow(C, 2));
+  v3_assign(Pn, A/mag, B/mag, C/mag);
+
+  
+  //Dot product of Pn and Rd to see if there is an intersection
+  Vd = v3_dot(Pn, Rd);
+  if (Vd == 0){
+    return(INFINITY);
+  }
+  if (Vd > 0){
+    double* temp = malloc(sizeof(double) * 3);
+    v3_sub(temp, Ro, obj.position);
+    double dot_temp1 = v3_dot(Pn, temp);
+    double t = -(dot_temp1/Vd);
+    if ( t < 0){
+      return(INFINITY);
+    }else{
+      return(t);
+    }
+  }
+}
